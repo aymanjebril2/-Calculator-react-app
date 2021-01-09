bfcddfebbb7f9ea7cbb60ff0c2a877081881614d
Ayman Omer <aymangebrilomer@gmail.com>
2021-01-09 01:51:50 -0500

    new update

--- src/componets/buttons/Value.js
+++ src/componets/buttons/Value.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import Buttons from "./Buttons";
 import "./Value.css";
-
+import Comma from "./Comma";
 const Value = () => {
   const [value, setValue] = useState("0");
   const [memory, setMemory] = useState(null);
   const [operator, setOperator] = useState(null);
 
   const handleButtonPress = (content) => () => {
-    let num = parseFloat(value);
+    const num = parseFloat(value);
 
     if (content === "AC") {
       setValue("0");
@@ -16,60 +16,125 @@ const Value = () => {
       setOperator(null);
       return;
     }
+
     if (content === "±") {
       setValue((num * -1).toString());
       return;
     }
+
     if (content === "%") {
       setValue((num / 100).toString());
       setMemory(null);
       setOperator(null);
       return;
     }
+
+    if (content === ".") {
+      if (value.includes(".")) return;
+
+      setValue(value + ".");
+      return;
+    }
+
     if (content === "+") {
-      setMemory(parseFloat(value));
+      if (operator !== null) {
+        if (operator === "+") {
+          setMemory(memory + parseFloat(value));
+        } else if (operator === "−") {
+          setMemory(memory - parseFloat(value));
+        } else if (operator === "×") {
+          setMemory(memory * parseFloat(value));
+        } else if (operator === "÷") {
+          setMemory(memory / parseFloat(value));
+        }
+      } else {
+        setMemory(parseFloat(value));
+      }
       setValue("0");
       setOperator("+");
       return;
     }
-    if (content === "-") {
-      setMemory(parseFloat(value));
+    if (content === "−") {
+      if (operator !== null) {
+        if (operator === "+") {
+          setMemory(memory + parseFloat(value));
+        } else if (operator === "−") {
+          setMemory(memory - parseFloat(value));
+        } else if (operator === "×") {
+          setMemory(memory * parseFloat(value));
+        } else if (operator === "÷") {
+          setMemory(memory / parseFloat(value));
+        }
+      } else {
+        setMemory(parseFloat(value));
+      }
       setValue("0");
-      setOperator("-");
-
+      setOperator("−");
       return;
     }
     if (content === "×") {
-      setMemory(parseFloat(value));
+      if (operator !== null) {
+        if (operator === "+") {
+          setMemory(memory + parseFloat(value));
+        } else if (operator === "−") {
+          setMemory(memory - parseFloat(value));
+        } else if (operator === "×") {
+          setMemory(memory * parseFloat(value));
+        } else if (operator === "÷") {
+          setMemory(memory / parseFloat(value));
+        }
+      } else {
+        setMemory(parseFloat(value));
+      }
       setValue("0");
       setOperator("×");
-
       return;
     }
     if (content === "÷") {
-      setMemory(parseFloat(value));
+      if (operator !== null) {
+        if (operator === "+") {
+          setMemory(memory + parseFloat(value));
+        } else if (operator === "−") {
+          setMemory(memory - parseFloat(value));
+        } else if (operator === "×") {
+          setMemory(memory * parseFloat(value));
+        } else if (operator === "÷") {
+          setMemory(memory / parseFloat(value));
+        }
+      } else {
+        setMemory(parseFloat(value));
+      }
       setValue("0");
-      setOperator("×");
+      setOperator("÷");
       return;
     }
+
     if (content === "=") {
       if (!operator) return;
+
       if (operator === "+") {
         setValue((memory + parseFloat(value)).toString());
-      } else if (operator === "-") {
+      } else if (operator === "−") {
         setValue((memory - parseFloat(value)).toString());
+      } else if (operator === "×") {
+        setValue((memory * parseFloat(value)).toString());
+      } else if (operator === "÷") {
+        setValue((memory / parseFloat(value)).toString());
       }
-
       setMemory(null);
       setOperator(null);
       return;
     }
 
-    setValue(parseFloat(num + content).toString());
+    if (value[value.length - 1] === ".") {
+      setValue(value + content);
+    } else {
+      setValue(parseFloat(num + content).toString());
+    }
   };
   return (
     <div className="value">
-      <div className="display">{value}</div>
+      <div className="display">{Comma(value)}</div>
       <div className="buttons">
         <Buttons
           onButtonClick={handleButtonPress}
@@ -104,7 +169,7 @@ const Value = () => {
         <Buttons onButtonClick={handleButtonPress} content="6" />
         <Buttons
           onButtonClick={handleButtonPress}
-          content="-"
+          content="−"
           type="operator"
         />
         <Buttons onButtonClick={handleButtonPress} content="1" />
